a3ddefcd140cf032d619357a265bcad75b6fdb78
theTrueMikeBrown <Thetruemikebrown@gmail.com>
2019-08-02 09:34:17 -0600

    check in

---
+++ public/precache-manifest.ba4217f462c6fd5d7e12ea0f02deecf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b7d3df93f246793fa2337f745300d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "513363a0e7b54333cb32",
+    "url": "/static/css/main.b7a3a5d8.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "513363a0e7b54333cb32",
+    "url": "/static/js/main.b469124a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
